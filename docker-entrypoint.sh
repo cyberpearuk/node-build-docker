84bf68fe4a360948c1db648b31df6487dbccff9b
James Buncle <jbuncle@hotmail.com>
2018-11-21 23:40:42 +0000

    Increased verbosity

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 set -e
 
+echo "NPM Registry: $NPM_REGISTRY"
+echo "NPM User: $NPM_USER"
+echo "NPM Pass: $NPM_PASS"
+
 # Check for required variables
 test -v NPM_REGISTRY || (echo "Missing NPM_REGISTRY variable" && exit 1)
 test -v NPM_USER     || (echo "Missing NPM_USER variable" && exit 1)
-test -v NPM_PASS     || (echo "Missing NPM_PASS variable" && exit 1)
+test -v NPM_PASS  || (echo "Missing NPM_PASS variable" && exit 1)
 
 NPM_AUTHTOKEN=$(echo -n "$NPM_USER:$NPM_PASSWORD" | openssl base64)
 
