397611ef5905baab2fb89a7b715a924c06e6174f
James Buncle <jbuncle@hotmail.com>
2018-11-22 00:17:52 +0000

    Added email to config + more verbose debug output

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,7 +11,9 @@ NPM_AUTHTOKEN=$(echo -n "$NPM_USER:$NPM_PASS" | openssl base64)
 cat << EOF > ~/.npmrc
 registry=$NPM_REGISTRY
 always-auth=true
+email=$GITLAB_USER_EMAIL
 _auth=$NPM_AUTHTOKEN
+
 EOF
 
 cat ~/.npmrc
@@ -19,5 +21,7 @@ cat ~/.npmrc
 # Print user and registry
 echo "User $(npm whoami)"
 echo "Registry $(npm config get registry)"
+echo "Email $(npm config get email)"
+echo "Auth $(npm config get _auth)"
 
 exec "$@"
